e18d93c63efa657a8307b125e9977965fed634c2
Florian Scholz <ww24e018@technikum-wien.at>
2025-03-24 11:21:51 +0100

    zwischenstand bsp loops

--- src/main/java/at/technikum/_Demos/InputLookI.java
+++ src/main/java/at/technikum/_Demos/InputLookI.java
@@ -15,10 +15,31 @@ public class InputLookI {
         */
 
         // how does a for look here?
-        for (int x2 = sc.nextInt(); x2 > 0 ; x2 = sc.nextInt()) {
+        /*for (int x2 = sc.nextInt(); x2 > 0 ; x2 = sc.nextInt()) {
             // do something
-        }
+        }*/
         // aw: still have to duplicate the nextInt()
+
+        // v1:
+        int num_inputs = -1;
+        int leinput;
+        do {
+            num_inputs++;
+            System.out.print(": ");
+            leinput = sc.nextInt();
+        } while (leinput > 0);
+        System.out.format("cnt: %d\n", num_inputs);
+        System.out.println("End.");
+
+        // and now ...
+        /*System.out.print(": ");
+        for (int ni = -1, lei = sc.nextInt() ; lei > 0 ; ni++, lei = sc.nextInt()) {
+            System.out.print(": ");
+        }
+        System.out.format("cnt: %d\n", num_inputs);
+        System.out.println("End.");*/
+        // nun; kann das do-while nicht ganz ersetzen, ":" fehlt; also die vollst. duplizierung des codeblocks.
+        // quick googling: kein is_undef() in java?
 
     }
 
